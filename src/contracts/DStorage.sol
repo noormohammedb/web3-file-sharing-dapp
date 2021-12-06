600cee1986bad261603628f3828478bc21bb5882
Noor Mohammed <mail@noormohammed.tech>
2021-12-06 11:52:14 +0530

    MVP

--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -1,37 +1,58 @@
 pragma solidity ^0.5.0;
 
 contract DStorage {
-  // Name
-  // Number of files
-  // Mapping fileId=>Struct 
+    // Name
+    string public name = "DStorage";
 
-  // Struct
+    // Number of files
+    uint256 public fileCount = 0;
 
+    // Mapping fileId=>Struct
+    mapping(uint256 => File) public allFiles;
 
-  // Event
+    // Struct
+    struct File {
+        uint256 fileId;
+        string fileHash;
+        uint256 fileSize;
+        string fileType;
+        string fileName;
+        string fileDescription;
+        uint256 uploadTime;
+        address payable uploader;
+    }
 
-  constructor() public {
-  }
+    // Event
 
-  // Upload File function
+    constructor() public {}
 
-    // Make sure the file hash exists
-
-    // Make sure file type exists
-
-    // Make sure file description exists
-
-    // Make sure file fileName exists
-
-    // Make sure uploader address exists
-
-    // Make sure file size is more than 0
-
-
-    // Increment file id
-
-    // Add File to the contract
-
-    // Trigger an event
-
+    // Upload File function
+    function uploadFile(
+        string memory fileHash,
+        uint256 fileSize,
+        string memory fileType,
+        string memory fileName,
+        string memory fileDescription
+    ) public {
+        fileCount++;
+        file[fileCount] = File(
+            fileCount,
+            fileHash,
+            fileSize,
+            fileType,
+            fileName,
+            fileDescription,
+            now,
+            msg.sender
+        );
+        // Make sure the file hash exists
+        // Make sure file type exists
+        // Make sure file description exists
+        // Make sure file fileName exists
+        // Make sure uploader address exists
+        // Make sure file size is more than 0
+        // Increment file id
+        // Add File to the contract
+        // Trigger an event
+    }
 }
