189c9772bc00cc69486e8fe4d7c8e3d18f510ce1
Noor Mohammed B <noormohammed.b@gpckasaragod.ac.in>
2022-03-13 18:41:44 +0530

    Updates

---
+++ precache-manifest.87530bebeb08b421878ff7408480a709.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2facf361b0483177bc19fccef760c09e",
+    "url": "/web3-file-sharing-dapp/index.html"
+  },
+  {
+    "revision": "7876e11563ffa4404bcb",
+    "url": "/web3-file-sharing-dapp/static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "f2e47f012cbd9dd28564",
+    "url": "/web3-file-sharing-dapp/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "7876e11563ffa4404bcb",
+    "url": "/web3-file-sharing-dapp/static/js/2.6512864a.chunk.js"
+  },
+  {
+    "revision": "b67f3bcb1e8c50265d3b21effa60c84d",
+    "url": "/web3-file-sharing-dapp/static/js/2.6512864a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2e47f012cbd9dd28564",
+    "url": "/web3-file-sharing-dapp/static/js/main.226b6be4.chunk.js"
+  },
+  {
+    "revision": "28c865af3e9b80bc5b2e",
+    "url": "/web3-file-sharing-dapp/static/js/runtime-main.5f96857b.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "/web3-file-sharing-dapp/static/media/box.df8b84d5.png"
+  }
+]);
